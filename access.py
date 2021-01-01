36b70c239c1a87ebb51942c117a8afd6155e7132
Janvi <janvit884@gmail.com>
2021-01-01 15:39:01 +0530

    added code file

---
+++ access.py
@@ -0,0 +1,21 @@
+
+
+import code as c
+
+
+
+c.create("janvi",22)
+# to create a key with key_name,value given and with no time-to-live property
+
+
+c.create("src", 70, 60)
+#to create a key with key_name,value given and with time-to-live property value given(number of seconds)
+
+
+c.read("janvi")
+
+
+
+
+
+
