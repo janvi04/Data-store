1b6bac66851315887320abc2b23d2287f9d7224b
Janvi <janvit884@gmail.com>
2021-01-01 15:54:17 +0530

    added code file

--- access.py
+++ access.py
@@ -1,4 +1,4 @@
-
+from threading import Thread
 
 import code as c
 
@@ -14,7 +14,12 @@ c.create("src", 70, 60)
 
 c.read("janvi")
 
-
+t1=Thread(target=(c.create or c.read or c.delete), args=(c.key_name, c.value, c.timeout)) #as per the operation
+t1.start()
+t1.sleep()
+t2=Thread(target=(c.create or c.read or c.delete),args=(c.key_name,c.value,c.timeout)) #as per the operation
+t2.start()
+t2.sleep()
 
 
 
